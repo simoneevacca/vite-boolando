7a28efebce4f0d2abf445f5d93fac3ac835ed712
simoneevacca <v.simone.vacca@gmail.com>
2024-03-21 16:44:38 +0100

    add all products

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,7 @@
     export default {
         name: 'AppHeader'
     }
-    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+    
    
 
 </script>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,18 +3,69 @@ export default {
     name: 'AppMain',
     data() {
         return {
-            message: 'hello world', 
+            message: 'hello world',
             products: [
                 {
                     firstImage: "/images/1.webp",
-                    secondImage: "1b.webp",
+                    secondImage: "/images/1b.webp",
                     discount: "-50%",
                     sostenibilita: true,
                     brand: "Levi's",
                     model: "RELAXED FIT TEE UNISEX",
-                    price: "14,99€",
-                    oldPrice: "25,99€"
-                }
+                    price: "14,99",
+                    oldPrice: "25,99"
+                },
+                {
+                    firstImage: "/images/2.webp",
+                    secondImage: "/images/2b.webp",
+                    discount: "-30%",
+                    sostenibilita: false,
+                    brand: "Guess",
+                    model: "ROSES TEE",
+                    price: "20,99",
+                    oldPrice: "29,99"
+                },
+                {
+                    firstImage: "/images/3.webp",
+                    secondImage: "/images/3b.webp",
+                    discount: "-30%",
+                    sostenibilita: false,
+                    brand: "Come Zucchero Filato",
+                    model: "VOGLIA DI COLORI PASTELLO",
+                    price: "129,99",
+                    oldPrice: "104,99"
+                },
+                {
+                    firstImage: "/images/4.webp",
+                    secondImage: "/images/4b.webp",
+                    discount: "-50%",
+                    sostenibilita: true,
+                    brand: "Levi's",
+                    model: "TEE UNISEX",
+                    price: "14,99",
+                    oldPrice: "29,99"
+                },
+                {
+                    firstImage: "/images/5.webp",
+                    secondImage: "/images/5b.webp",
+                    discount: null,
+                    sostenibilita: false,
+                    brand: "Maya Deluxe",
+                    model: "STRIPE BODICE",
+                    price: "99,99",
+                    oldPrice: null
+                },
+                {
+                    firstImage: "/images/6.webp",
+                    secondImage: "/images/6b.webp",
+                    discount: null,
+                    sostenibilita: true,
+                    brand: "Levi's",
+                    model: "RELAXED FIT TEE UNISEX",
+                    price: "29,99",
+                    oldPrice: null
+                },
+
             ],
         }
     }
@@ -24,7 +75,32 @@ export default {
 </script>
 
 <template>
-{{ message }}
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-4" v-for="product in products">
+                    <div class="img">
+                        <img :src="product.firstImage" alt="">
+                        <div class="banner">
+                            <div class="discount" v-if="product.discount">{{product.discount}}</div>
+                            <div v-show="product.sostenibilita == true" class="sustainability">Sostenibilità</div>
+                        </div>
+                        <div class="heart">&hearts;</div>
+                    </div>
+                    <div class="text">
+                        <div class="brand">{{product.brand}}</div>
+                        <div class="name"><strong>{{product.model}}</strong></div>
+                        <div class="price">
+                            <span class="actual_price">{{product.price}} &euro; </span>
+                            <span class="old_price" v-if="product.discount"> {{product.oldPrice}} &euro;</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
 </template>
 
 <style></style>
