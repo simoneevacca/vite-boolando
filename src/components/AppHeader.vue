2f8b4c00ddba399c77c788370ba8a265ff1187e0
simoneevacca <v.simone.vacca@gmail.com>
2024-03-21 15:15:08 +0100

    add header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,7 +5,27 @@
 </script>
 
 <template>
-  <div>test header</div>
+   <header id="site_header">
+        
+            <nav class="nav_container">
+                <div class="link">
+                    <ul class="ul_nav">
+                        <li><a class="gender" href="#">Donna</a></li>
+                        <li><a class="gender" href="#">Umomo</a></li>
+                        <li><a class="gender" href="#">Bambino</a></li>
+                    </ul>
+                </div>
+                <div class="logo">
+                    <img width="200px" src="/images/boolean-logo.png" alt="">
+                </div>
+                <div class="user">
+                    <i class="fa-regular fa-user"></i>
+                    <i class="fa-regular fa-heart"></i>
+                    <i class="fa-solid fa-bag-shopping"></i>
+                </div>
+            </nav>
+        
+    </header>
 </template>
 
 <style>
