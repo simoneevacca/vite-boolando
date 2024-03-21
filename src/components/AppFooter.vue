527981a4790189ff1b03426cf909ab04df0f2647
simoneevacca <v.simone.vacca@gmail.com>
2024-03-21 20:28:03 +0100

    add footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,35 @@
 <script>
-    export default {
-        name: 'AppFooter'
-    }
+export default {
+  name: 'AppFooter'
+}
 </script>
 
 <template>
-  <div>test footer</div>
+  <footer class="site_footer">
+    <div class="foot container">
+      <div class="foot">
+        <div class="society">Boolando s.r.l.</div>
+        <div class="privacy">
+          <ul class="ul_footer">
+            <li><a class="info" href="#">Iformazioni legali</a></li>
+            <li><a class="info" href="#">Informativa sulla privacy</a></li>
+            <li><a class="info" href="#">Diritto di recesso</a></li>
+          </ul>
+        </div>
+
+      </div>
+      <div class="social">
+        <div class="find_us">Trovaci anche su</div>
+        <div class="social_icon">
+          <i class="fa-brands fa-square-twitter"></i>
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-pinterest"></i>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
-<style>
-
-</style>
+<style></style>
