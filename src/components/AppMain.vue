8907048198487dbdb95c38997be7278c6a1099bc
simoneevacca <v.simone.vacca@gmail.com>
2024-03-25 14:59:07 +0100

    add file state.js for globalize data read

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,7 @@
 <script>
 import productCard from './productCard.vue';
-// import { products } from '../data.js';
+
 import { state } from '../state.js';
-import axios from 'axios';
 
 export default {
     name: 'AppMain',
@@ -11,21 +10,15 @@ export default {
     },
     data() {
         return {
-            // products,
+         
             state,
-            products: []
         }
     },
     mounted () {
 
-        
+        state.GetProduct('http://localhost:3000/products')
 
-        axios.get('http://localhost:3000/products')
-        .then(response => {
-          console.log(response)
-          this.products = response.data
-        })
-        console.log(this.products);
+       
     }
 }
 
@@ -37,7 +30,7 @@ export default {
         <div class="container">
             <div class="row justify-content-between">
 
-                <productCard :product="product" :key="product.id" v-for="product in products" />
+                <productCard :product="product" :key="product.id" v-for="product in state.products" />
 
             </div>
         </div>
--- src/state.js
+++ src/state.js
@@ -1,4 +1,18 @@
 import { reactive } from 'vue';
+import axios from 'axios';
+
 export const state = reactive ({
-    
+    products: [],
+
+GetProduct(url) {
+
+    axios.get(url)
+    .then(response => {
+      console.log(response)
+      this.products = response.data
+    }),
+    console.log(this.products);
+}
 })
+
+
