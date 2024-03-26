6b0621bac6edab81cef13c6c37fc7ab9ebd2aeed
simoneevacca <v.simone.vacca@gmail.com>
2024-03-26 20:32:22 +0100

    fix general, add functions comment and formatting code

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,22 +8,28 @@ export default {
     components: {
         productCard
     },
+
     data() {
         return {
 
             state,
 
+            // declaration of properties to be inserted in the modal 
             isActive: false,
             brand: '',
             model: '',
             price: '',
             firstImage: '',
             secondImage: '',
-
         }
     },
 
     methods: {
+
+        /**
+         * show the modal with and fills in the product data
+         * @param {number} id 
+         */
         modale(id) {
             this.isActive = true
             console.log('ciao', id);
@@ -40,53 +46,44 @@ export default {
             console.log(this.isActive);
         }
     },
+
     mounted() {
 
         state.GetProduct('http://localhost:3000/products')
 
-
-
     }
 }
-
-
 </script>
 
 <template>
     <main>
+
         <div class="container">
             <div class="row justify-content-between">
                 <div class="col col-4" v-for="product in state.products">
 
                     <productCard @performModale="modale" :product="product" :key="product.id" />
 
-
-
                 </div>
-
             </div>
         </div>
 
-        <div :class="{ modale: isActive }" v-show="isActive == true">
+        <div class="modale" v-show="isActive == true">
             <div class="close" @click="closeModale">X</div>
             <div class="content">
                 <div class="left">
                     <img :src="firstImage" alt="">
                     <img :src="secondImage" alt="">
-                    
                 </div>
                 <div class="right">
                     <div>Marca: {{ brand }}</div>
                     <div>Modello: {{ model }}</div>
-                    <div>Prezzo: {{ price }}€</div>
+                    <div>Prezzo: {{ price }}&euro;</div>
                 </div>
             </div>
-
-
         </div>
+
     </main>
-
-
 </template>
 
 <style></style>
--- src/components/productCard.vue
+++ src/components/productCard.vue
@@ -4,9 +4,7 @@ export default {
     emits: ['performModale'],
     props: {
         product: Object,
-        
     },
-   
 
     methods: {
         modale() {
@@ -17,26 +15,23 @@ export default {
 </script>
 
 <template>
-
-        <div class="img">
-            <img :src="product.firstImage" alt="">
-            <img class="hover_fig" :src="product.secondImage" alt="">
-            <div class="banner">
-                <div class="discount" v-if="product.discount">{{ product.discount }}</div>
-                <div v-show="product.sostenibilita == true" class="sustainability">Sostenibilità</div>
-            </div>
-            <div class="heart">&hearts;</div>
+    <div class="img">
+        <img :src="product.firstImage" alt="">
+        <img class="hover_fig" :src="product.secondImage" alt="">
+        <div class="banner">
+            <div class="discount" v-if="product.discount">{{ product.discount }}</div>
+            <div v-show="product.sostenibilita == true" class="sustainability">Sostenibilità</div>
         </div>
-        <div class="text">
-            <div class="brand">{{ product.brand }}</div>
-            <div class="name" @click="modale"><strong>{{ product.model }}</strong></div>
-            <div class="price">
-                <span class="actual_price">{{ product.price }} &euro; </span>
-                <span class="old_price" v-if="product.discount"> {{ product.oldPrice }} &euro;</span>
-            </div>
+        <div class="heart">&hearts;</div>
+    </div>
+    <div class="text">
+        <div class="brand">{{ product.brand }}</div>
+        <div class="name" @click="modale"><strong>{{ product.model }}</strong></div>
+        <div class="price">
+            <span class="actual_price">{{ product.price }} &euro; </span>
+            <span class="old_price" v-if="product.discount"> {{ product.oldPrice }} &euro;</span>
         </div>
-       
-
+    </div>
 </template>
 
 <style></style>
